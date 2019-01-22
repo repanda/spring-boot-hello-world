11480a6d905961b32c2c11c4c47b0ca99a07483d
khaled boussaidi <b.khaled.mi@gmail.com>
2019-01-22 14:45:57 +0100

    delete méthode

--- src/main/java/com/patrickgrimard/Application.java
+++ src/main/java/com/patrickgrimard/Application.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 
 /**
@@ -19,9 +17,5 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
-    //@EventListener(ApplicationReadyEvent.class)
-    public void afterRun(){
-        System.out.println("hello world!!!");
-    }
+   
 }
