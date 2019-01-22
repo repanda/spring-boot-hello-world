c32d781a1536e90fa5f41774702fd8b425b3fd5c
khaled boussaidi <b.khaled.mi@gmail.com>
2019-01-22 14:26:07 +0100

    Update

--- src/main/java/com/patrickgrimard/Application.java
+++ src/main/java/com/patrickgrimard/Application.java
@@ -16,4 +16,9 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @EventListener(ApplicationReadyEvent.class)
+    public void afterRun(){
+        System.out.printLine("hello world!!!")
+    }
 }
