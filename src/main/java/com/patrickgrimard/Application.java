08288eafbab2b6a55b82cf50863589fcac45fca4
khaled boussaidi <b.khaled.mi@gmail.com>
2019-01-22 14:37:00 +0100

    correction

--- src/main/java/com/patrickgrimard/Application.java
+++ src/main/java/com/patrickgrimard/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
 
 /**
  * Created by XTL on 8/14/2014.
